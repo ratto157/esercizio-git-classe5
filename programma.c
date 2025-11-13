771c624fbf75a393c8781768566a3f4af1f7c813
CALVINO\I5DT14 <fabio.ratto.2007@calvino.edu.it>
2025-11-13 16:23:14 +0100

    aggiunta funzione somma

--- programma.c
+++ programma.c
@@ -1,5 +1,14 @@
+<stdio.h>
+
 main(){
-	printf("Hello World");
-	printf("Fabio Ratto");
+	int x=0;
+	int y=0;
+	printf("Hello World\n");
+	printf("Fabio Ratto\n");
+	printf("numero1\n");
+	scanf("%d",&x);
+	printf("numero2\n");
+	scanf("%d",&y);
+	printf("somma: %d", x+y);
 	return 0;
 }
